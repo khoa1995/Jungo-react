fd776d23dfadab414d18c5af0f569b31ed0e2da2
phannguyendangkhoa1995@gmail.com <phannguyendangkhoa1995@gmail.com>
2020-04-10 23:16:44 +0700

    first commit

---
+++ src/components/Menu/menu.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './menu.scss'
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="menu-wrapper">
+                <div>Hello world</div>
+            </div>
+         );
+    }
+}
+ 
+export default Menu;
+
+
